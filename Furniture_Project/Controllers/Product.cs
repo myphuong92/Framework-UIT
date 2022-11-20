3fd2481a5ef2b3363828b9629a673efe4ae034d2
Phương <20521779@gm.uit.edu.vn>
2022-11-20 14:45:01 +0700

    layout and product page

---
+++ Furniture_Project/Controllers/Product.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Furniture_Project.Controllers
+{
+    public class Product : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
