3d8a32898508f54effb20101774dd812ec519a0f
Đoàn Tú Quỳnh <101488005+Quynh-1825@users.noreply.github.com>
2022-12-24 02:35:05 +0700

    commit admin/TTDH

--- Funiture_Project/Areas/Admin/Controllers/DonHangController.cs
+++ Funiture_Project/Areas/Admin/Controllers/DonHangController.cs
@@ -4,15 +4,19 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Collections.Generic;
 using System;
+using AspNetCoreHero.ToastNotification.Notyf;
+using AspNetCoreHero.ToastNotification.Abstractions;
 
 namespace Funiture_Project.Areas.Admin.Controllers
 {
     public class DonHangController : Controller
     {
         private readonly FurnitureContext _context;
-        public DonHangController(FurnitureContext context)
+        public INotyfService _notyfService { get; }
+        public DonHangController(FurnitureContext context, INotyfService notyfService)
         {
             _context = context;
+            _notyfService = notyfService;
         }
         [Area("Admin")]
         public IActionResult DangCho()
@@ -24,9 +28,9 @@ namespace Funiture_Project.Areas.Admin.Controllers
             {
                 var khachhang = _context.KhachHang.AsNoTracking()
                     .Where(x => x.MaKh == item.MaKh)
-                    .Select(x=>x.HoTen)
+                    .Select(x => x.HoTen)
                     .FirstOrDefault();
-                if(khachhang!=null)
+                if (khachhang != null)
                 {
                     lsKhachHang.Add(khachhang);
                 }
@@ -139,10 +143,71 @@ namespace Funiture_Project.Areas.Admin.Controllers
         }
 
         [Area("Admin")]
-        public IActionResult DangCho_Update(string mahd)
+        [Route("/DangCho_Update/{mahd}", Name = "DangCho_Update")]
+        public IActionResult DangCho_Update(int mahd)
         {
-
+            var hoadon = _context.HoaDon.AsNoTracking()
+                .Where(x => x.MaHd == mahd)
+                .FirstOrDefault();
+            var khachhang = _context.KhachHang.AsNoTracking()
+                .Where(x => x.MaKh == hoadon.MaKh)
+                .FirstOrDefault();
+            var cthd = _context.Cthd.AsNoTracking()
+                .Where(x => x.MaHd == mahd)
+                .ToList();
+            List<SanPham> lsSanPham = new List<SanPham>();
+            foreach (var item in cthd)
+            {
+                var sanpham = _context.SanPham.AsNoTracking()
+                .Where(x => x.MaSp == item.MaSp)
+                .FirstOrDefault();
+                lsSanPham.Add(sanpham);
+            }
+            ViewBag.HoaDon = hoadon;
+            ViewBag.KhachHang = khachhang;
+            ViewBag.CTHD = cthd;
+            ViewBag.lsSanPham = lsSanPham;
             return View();
+        }
+
+        [Area("Admin")]
+        [Route("/HuyHoaDon/{mahd}", Name = "Cancel_Order")]
+        public IActionResult HuyHoaDon(int mahd)
+        {
+            var hoadon = _context.HoaDon.AsNoTracking()
+                .Where(x => x.MaHd == mahd)
+                .FirstOrDefault();
+            hoadon.Ttdh = "Bị hủy";
+            _context.HoaDon.Update(hoadon);
+            _context.SaveChanges();
+            _notyfService.Success("Hủy hóa đơn thành công");
+            return RedirectToAction("DangCho", "DonHang");
+        }
+
+        [Area("Admin")]
+        [Route("/CapNhatTrangThai/{mahd}", Name = "CapNhatTrangThai")]
+        public IActionResult CapNhatTrangThai(int mahd)
+        {
+            var hoadon = _context.HoaDon.AsNoTracking()
+                .Where(x => x.MaHd == mahd)
+                .FirstOrDefault();
+            if (hoadon.Ttdh.Equals("Đang chờ"))
+            {
+                hoadon.Ttdh = "Đang giao";
+            }
+            else 
+            {
+                if (hoadon.Ttdh.Equals("Đang giao"))
+                {
+                    hoadon.Ttdh = "Đã giao";
+                }
+            } 
+            _context.HoaDon.Update(hoadon);
+            _context.SaveChanges();
+
+            _notyfService.Success("Cập nhật trạng thái thành công");
+
+            return RedirectToAction("DangCho","DonHang");
         }
     }
 }
